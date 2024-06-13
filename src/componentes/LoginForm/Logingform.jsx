0a66e05fd7b2ca1b9678da4cd8b83a8bdc62911c
Nan Barrera <nanbarrera14@gmail.com>
2024-06-13 16:33:35 -0600

    Interfaz venta

--- src/componentes/LoginForm/Logingform.jsx
+++ src/componentes/LoginForm/Logingform.jsx
@@ -1,36 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import './LoginForm.css';
-import {FaUser, FaLock} from "react-icons/fa";
+import { FaUser, FaLock } from "react-icons/fa";
 import logo from './../Assets/logo.png';
+import Sidebar from "../venta/Sidebar"; // Ruta ajustada para acceder al componente Sidebar
 
 const LoginForm = () => {
-    return(
+    const [username, setUsername] = useState("admin");
+    const [password, setPassword] = useState("123456");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        // Aquí iría tu lógica de autenticación, como consultar una base de datos o verificar las credenciales
+        // Por simplicidad, aquí solo comprobaremos que el usuario y la contraseña no estén vacíos
+        if (username && password) {
+            setIsLoggedIn(true); // Establecer el estado de inicio de sesión como verdadero
+        } else {
+            alert("Por favor, introduce un nombre de usuario y una contraseña.");
+        }
+    }
+
+    return isLoggedIn ? (
+        <Sidebar />
+    ) : (
         <div className="wrapper">
             <h4 className="tituloTurno">Comenzar Nuevo Turno</h4>
             <div className="logoPrincipal">
                 <h3 className="nombreEmpresa">EL TOTO</h3>
-                <img className="imgLogo" src={logo} />
+                <img className="imgLogo" src={logo} alt="logo" />
             </div>
-            
-            <form action="">
-                
+
+            <form onSubmit={handleLogin}>
                 <div className="input-box">
-                    <input type="text" placeholder="Username" required/>
+                    <input type="text" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)} />
                     <FaUser className="icon" />
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder="Password" required/>
+                    <input type="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)} />
                     <FaLock className="icon" />
                 </div>
 
                 <div className="botones">
                     <button className="acceder" type="submit">Acceder</button>
-                    <button className="salir" type="submit">Salir</button>
+                    {/* No necesitas un botón de "Salir" en el formulario de inicio de sesión */}
                 </div>
-
             </form>
         </div>
-    )
+    );
 }
 
 export default LoginForm;
--- src/componentes/venta/Sidebar.js
+++ src/componentes/venta/Sidebar.js
@@ -1,91 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import './Sidebar.css';
 import { SidebarData } from './SidebarData';
 
 
 
+
+
 function Sidebar() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [products, setProducts] = useState([
+        { id: 1, code: "123456789", description: "Coca-Cola 500 ML", price: 20.00, quantity: 1 },
+        { id: 2, code: "098765432", description: "Chetos", price: 15.00, quantity: 1 }
+    ]);
+
+    const handleLogout = () => {
+        // Lógica para salir
+        window.location.pathname = "/login"; // Redirige al usuario al formulario de inicio de sesión
+    }
+
     return (
-    <div className="principal">
-        <div className="container">
-        
-            <div className="Sidebar">
-                <ul className="SidebarList">
-                    {SidebarData.map((val, key) => (
-                        <li
-                            key={key}
-                            className="row"
-                            onClick={() => { window.location.pathname = val.link; }}
-                        >
-                            <div id="icon">{val.icon}</div>
-                            <div id="title">{val.title}</div>
-                        </li>
-                    ))}
-                </ul>
-                <div><button className="logoutButton" onClick={() => { /* Logica para salir */ }}>
-                    Salir
-                </button></div>
-            </div>
-            
-            <div className="content">
-                <div className="header-container">
-                    <h1 className="header">Venta</h1>
+        <div className="principal">
+            <div className="container">
+
+                <div className="Sidebar">
+                    <ul className="SidebarList">
+                        {SidebarData.map((val, key) => (
+                            <li
+                                key={key}
+                                className="row"
+                                onClick={() => { window.location.pathname = val.link; }}
+                            >
+                                <div id="icon">{val.icon}</div>
+                                <div id="title">{val.title}</div>
+                            </li>
+                        ))}
+                    </ul>
+                    <div><button className="logoutButton" onClick={handleLogout}>
+                        Salir
+                    </button></div>
+
                 </div>
-                <div className="main-content">
-                    <div className="search-bar">
-                        <label htmlFor="search">Nombre del producto:</label>
-                        <input type="text" id="search" name="search" placeholder="Buscar..." />
-                        <button className="search-button"><i className="fas fa-search"></i></button>
-                        <button className="add-button">Agregar</button>
-                    </div>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Código del producto</th>
-                                <th>Descripción del producto</th>
-                                <th>Precio de venta</th>
-                                <th>Cantidad</th>
-                                <th>Acción</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>123456789</td>
-                                <td>Coca-Cola 500 ML</td>
-                                <td>$20.00</td>
-                                <td>1</td>
-                                <td><i className="fas fa-trash-alt"></i></td>
-                            </tr>
-                            <tr>
-                                <td>098765432</td>
-                                <td>Chetos</td>
-                                <td>$15.00</td>
-                                <td>1</td>
-                                <td><i className="fas fa-trash-alt"></i></td>
-                            </tr>
 
-                        </tbody>
-                    </table>
-                    <div className="total-section">
-                        <div className="payment-details">
-                            <div className="payment-item">Total: </div>
-                            <div className="payment-item">Pago efectivo: </div>
-                            <div className="payment-item">Cambio: </div>
-                        </div>
-                        <div className="button-group">
-                            <button className="cancel-button">Cancelar</button>
-                            <button className="checkout-button">Cobrar</button>
-                        </div>
-                        <div className="amount-to-pay">
-                            <div className="amount-value">$35.00</div>
-                        </div>
-                        <button className="print-button">Imprimir ticket</button>
+                <div className="content">
+                    <div className="header-container">
+                        <h1 className="header">Venta</h1>
                     </div>
-                    </div>
-               
+                    <div className="main-content">
+                        <div className="search-bar">
+                            <label htmlFor="search">Nombre del producto:</label>
+                            <input type="text" id="search" name="search" placeholder="Buscar..." />
+                            <button className="search-button"><i className="fas fa-search"></i></button>
+                            <button className="add-button">Agregar</button>
+                        </div>
+                        <div className="tabla">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>Código del producto</th>
+                                        <th>Descripción del producto</th>
+                                        <th>Precio de venta</th>
+                                        <th>Cantidad </th>
+                                        <th>Acción</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>123456789</td>
+                                        <td>Coca-Cola 500 ML</td>
+                                        <td>$20.00</td>
+                                        <td>1</td>
+                                        <td><i className="fas fa-trash-alt"></i></td>
+                                    </tr>
+                                    <tr>
+                                        <td>098765432</td>
+                                        <td>Chetos</td>
+                                        <td>$15.00</td>
+                                        <td>1</td>
+                                        <td><i className="fas fa-trash-alt"></i></td>
+                                    </tr>
 
+                                </tbody>
+                            </table>
+                        </div>
+                        
+
+                        <div className="total-section">
+                            <div className="payment-details">
+                                <div className="payment-item">
+                                    <div>Total:</div>
+                                    <div>$35.00</div>
+                                </div>
+                                <div className="payment-item">
+                                    <div>Pago efectivo:</div>
+                                    <div>$50.00</div>
+                                </div>
+                                <div className="payment-item">
+                                    <div>Cambio:</div>
+                                    <div>$15.00</div>
+                                </div>
+                            </div>
+                            <div className="button-group">
+                                <button className="checkout-button">Cobrar</button>
+                                <button className="cancel-button">Cancelar</button>
+                            </div>
+                            <div className="amount-to-pay">
+                                <div className="amount-value">$35.00</div>
+                                <button className="print-button">Imprimir ticket</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
             </div>
-        </div>
         </div>
     );
 }
